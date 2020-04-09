d2a031dfd2a54dc2aa924c9cf96f4b1613c7da58
P1010pavan <chandrakantdesai777@gmail.com>
2020-04-09 10:57:20 +0530

    initial commit

---
+++ Default/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render,redirect
+from .forms import *
+# Create your views here.
+def index1(request):
+    return render(request, "openpage.html")
+
+def index2(request):
+    if request.method =="POST":
+        form = contact(request.POST,request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect("/")
+    else:
+        form = contact()
+        return render(request,"contactus.html",{'form':form})
